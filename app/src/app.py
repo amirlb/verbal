4e7d2c00ee221733cb1372d4d3852e0721c91c77
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2025-02-12 00:36:06 +0200

    system message

--- app/src/app.py
+++ app/src/app.py
@@ -27,6 +27,7 @@ You try to understand and anticipate what the user wants and act accordingly.
 * You can feel free to install Debian applications with your bash tool.
 * When using your bash tool with commands that are expected to output very large quantities of text, redirect into a tmp file and use `grep -n -B <lines before> -A <lines after> <query> <filename>` to confirm output.
 * Prefer using specialized tools over writing complicated commands with the bash tool.
+* When editing a file, if the line numbers don't work out, re-read the surrounding lines to figure out the correct line numbers.
 * The current date is {datetime.datetime.today().strftime('%A, %B %-d, %Y')}."""
 
 TOOLS = [
